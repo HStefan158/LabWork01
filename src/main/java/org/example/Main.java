91e49d4d6403459b56f866ef261645dd8c7d323d
Hoțea Stefan-Alexandru <hoteastefan340@gmail.com>
2026-05-11 10:46:36 +0300

    afisareNumere

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        afiseazaNumere();
+    }
+
+    public static void afiseazaNumere() {
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i);
+        }
+    }
+}
