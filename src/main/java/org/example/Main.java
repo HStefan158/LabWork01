f97601e24fa6934de12d37c6b22093cbac3005e3
Hoțea Stefan-Alexandru <hoteastefan340@gmail.com>
2026-05-11 10:57:31 +0300

    NumerePareSiImpare

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,20 @@ public class Main {
     public static void main(String[] args) {
 
         afiseazaNumere();
+        System.out.println();
+        afiseazaNumerePare();
+        System.out.println();
+
+        int[] a = new int [100];
+        for( int i = 0;i<100;i++)
+        {
+            a[i] = i;
+            System.out.println(a[i]);
+        }
+
+        extrageImpare(a);
+        extragePare(a);
+
     }
 
     public static void afiseazaNumere() {
@@ -13,9 +27,20 @@ public class Main {
     }
 
     public static void afiseazaNumerePare() {
-        // Pornim de la 2 și adunăm 2 la fiecare pas (i += 2)
         for (int i = 2; i <= 100; i += 2) {
             System.out.println(i);
+        }
+    }
+
+    public static void extrageImpare(int[] a) {
+        for (int i = 0; i < a.length; i += 2) {
+            System.out.println(a[i]);
+        }
+    }
+
+    public static void extragePare(int[] a) {
+        for (int i = 0; i < a.length; i += 2) {
+            System.out.println(a[i]);
         }
     }
 }
