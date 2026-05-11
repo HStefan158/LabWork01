fa91edb442568737eb29d0971448d9ce3a87dd4a
Hoțea Stefan-Alexandru <hoteastefan340@gmail.com>
2026-05-11 10:47:31 +0300

    afisareNumerePare

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,4 +11,11 @@ public class Main {
             System.out.println(i);
         }
     }
+
+    public static void afiseazaNumerePare() {
+        // Pornim de la 2 și adunăm 2 la fiecare pas (i += 2)
+        for (int i = 2; i <= 100; i += 2) {
+            System.out.println(i);
+        }
+    }
 }
